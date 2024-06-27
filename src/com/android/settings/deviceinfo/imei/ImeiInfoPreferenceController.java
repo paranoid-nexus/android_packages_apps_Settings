6cee669bb0df56906a01132a1bca9a175d9f4ef6
althafvly <althafvly@gmail.com>
2024-06-27 21:19:52 +0530

    Revert "[Sim UI enhancement] remove the "Tap to show info""
    This reverts commit c6eba747db60b044999e29bbf96643eb41799c3c.
    
    Change-Id: Ib8403ca842a4655d1526b27f1965feeee336c687

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -42,6 +42,7 @@ import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.PhoneNumberSummaryPreference;
 import com.android.settings.deviceinfo.simstatus.SlotSimStatus;
 import com.android.settings.network.SubscriptionUtil;
 import com.android.settings.network.telephony.TelephonyUtils;
@@ -121,7 +122,6 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             multiImeiPreference.setKey(DEFAULT_KEY + (1 + simSlotNumber));
             multiImeiPreference.setEnabled(true);
             multiImeiPreference.setCopyingEnabled(true);
-
             category.addPreference(multiImeiPreference);
             if (mIsDsdsToSsConfigValid) {
                 multiImeiPreference.setTitle(getTitle(simSlotNumber));
@@ -176,6 +176,11 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
         updatePreference(preference, keyToSlotIndex(preference.getKey()));
     }
 
+    @Override
+    public CharSequence getSummary() {
+        return mContext.getString(R.string.device_info_protected_single_press);
+    }
+
     private CharSequence getSummary(int simSlot) {
         final int phoneType = getPhoneType(simSlot);
         if (Utils.isSupportCTPA(mContext)) {
@@ -220,12 +225,8 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
 
     @VisibleForTesting
     protected void updatePreference(Preference preference, int simSlot) {
-        if (simSlot < 0) {
-            preference.setVisible(false);
-            return;
-        }
         preference.setTitle(getTitle(simSlot));
-        preference.setSummary(getSummary(simSlot));
+        preference.setSummary(getSummary());
     }
 
     private String getImei(int slot) {
@@ -341,7 +342,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
 
     @VisibleForTesting
     Preference createNewPreference(Context context) {
-        return new Preference(context);
+        return new PhoneNumberSummaryPreference(context);
     }
 
     private int makeRadioVersion(int major, int minor) {
